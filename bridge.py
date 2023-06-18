7c4c2dbe20a39cb70a27666a77655fc75e7fff7b
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-06-18 14:00:00 +0200

    Enhance smart contract state changes monitoring

--- bridge.py
+++ bridge.py
@@ -1,3 +1,4 @@
 // Improve staking logic for better rewards
 // Add non-fungible token (NFT) support
 // Integrate automated market maker (AMM) logic
+// Add support for zk-SNARKs in smart contracts
