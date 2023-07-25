9fe61f9289e1d2e6f3162708cad53280b34899f9
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-07-25 14:00:00 +0200

    Refactor token economics model

--- decentralized_exchange.py
+++ decentralized_exchange.py
@@ -1,3 +1,4 @@
 // Add support for decentralized lending platforms
 // Add support for fractional token ownership
 // Improve oracle data validation mechanisms
+// Refactor contract to use EIP-712 signatures
