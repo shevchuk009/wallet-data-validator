cb7e54f93018036f6e868bd2da8eb14c7b4292e1
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-09-08 14:00:00 +0200

    Enhance privacy features using zk-STARKs

--- reward_pooling.sol
+++ reward_pooling.sol
@@ -2,3 +2,4 @@
 // Enhance governance contract flexibility
 // Refactor decentralized exchange (DEX) code
 // Implement a new contract event system
+// Improve decentralized lending contract flexibility
