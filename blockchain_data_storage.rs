c7bda3c78bbe9da4a3f26ab711a4752793686d97
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-10-12 14:00:00 +0200

    Fix issues with token vesting contracts

--- blockchain_data_storage.rs
+++ blockchain_data_storage.rs
@@ -2,3 +2,4 @@
 // Add support for decentralized credit scoring
 // Refactor decentralized exchange (DEX) code
 // Enhance smart contract upgradeability
+// Integrate with Ethereum Layer-2 solutions
