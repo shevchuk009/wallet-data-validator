39bc987643ab18149b8217314db18a8ec6a10684
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-10-30 14:00:00 +0200

    Fix issue in ERC-20 token transfers

--- voting_mechanism.go
+++ voting_mechanism.go
@@ -1,3 +1,4 @@
 // Implement contract function batching
 // Fix issues with cross-chain transaction finality
 // Fix errors in contract interaction with wallets
+// Fix vulnerabilities in staking contracts
