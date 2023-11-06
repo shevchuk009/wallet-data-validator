8072b841ec46dc851767f878744e5cebda5bd052
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-11-06 14:00:00 +0200

    Improve multi-chain contract deployment

--- voting_mechanism.go
+++ voting_mechanism.go
@@ -2,3 +2,4 @@
 // Fix issues with cross-chain transaction finality
 // Fix errors in contract interaction with wallets
 // Fix vulnerabilities in staking contracts
+// Fix memory leak in Rust smart contract
