018a0dff43e9a114f1c47e250d4a195d42c238bb
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-11-08 14:00:00 +0200

    Fix issues with contract finalization

--- transaction_liveness.rs
+++ transaction_liveness.rs
@@ -1,3 +1,4 @@
 // Refactor contract state machine design
 // Improve token burning mechanism
 // Fix issues with gas optimization in smart contracts
+// Add support for zk-SNARKs in smart contracts
