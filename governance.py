ba5125ca26b217d15394d1b6a19fbdfac3723b73
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-11-20 14:00:06 +0200

    Update smart contract tests

--- governance.py
+++ governance.py
@@ -2,3 +2,4 @@
 // Add support for decentralized lending platforms
 // Implement decentralized arbitration in contracts
 // Fix type mismatch in Haskell smart contract
+// Fix compatibility issues with major wallets
