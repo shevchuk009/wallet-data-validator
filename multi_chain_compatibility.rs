f7c3b523ba175691a5a8431745554712d7f52dc7
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2023-12-11 14:00:00 +0200

    Fix bugs in NFT metadata handling

--- multi_chain_compatibility.rs
+++ multi_chain_compatibility.rs
@@ -1,3 +1,4 @@
 // Fix governance proposal approval logic
 // Fix errors in contract interaction with wallets
 // Enhance smart contract state changes monitoring
+// Fix issue in token vesting logic
