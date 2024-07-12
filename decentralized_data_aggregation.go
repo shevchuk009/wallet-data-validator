60e82fafbfe4f26c553d56b6a8775e9fd56ce107
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2024-07-12 14:00:00 +0200

    Optimize hashing algorithm

--- decentralized_data_aggregation.go
+++ decentralized_data_aggregation.go
@@ -2,3 +2,4 @@
 // Integrate decentralized prediction models
 // Refactor smart contract testing framework
 // Fix data consistency issues in smart contracts
+// Fix overflow bug in token calculation
