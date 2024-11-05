bc05ae480bbf1d0aeeaa0a11d52e2500f67ffa7a
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2024-11-05 14:00:00 +0200

    Fix overflow bug in token calculation

--- multi_sig_escrow_services.rs
+++ multi_sig_escrow_services.rs
@@ -1,3 +1,4 @@
 // Improve transaction batching efficiency
 // Fix deserialization bug in Haskell code
 // Fix type mismatch in Haskell smart contract
+// Add support for decentralized lending platforms
