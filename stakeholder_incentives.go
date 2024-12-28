87bc15936daf5507e722e64a6c6b2beb00280680
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2024-12-28 14:00:00 +0200

    Integrate IPFS for storage

--- stakeholder_incentives.go
+++ stakeholder_incentives.go
@@ -1,3 +1,4 @@
 // Implement liquidity pool staking mechanism
 // Improve scalability for smart contract platforms
 // Fix type mismatch in Haskell smart contract
+// Optimize WASM compilation for Go contracts
