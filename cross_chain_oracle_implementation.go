ecf52dcc8df6bf91c9c725863a265696526704e4
shevchuk009 <143076131+shevchuk009@users.noreply.github.com>
2025-01-20 14:00:01 +0200

    Refactor reward pooling algorithm

--- cross_chain_oracle_implementation.go
+++ cross_chain_oracle_implementation.go
@@ -1,3 +1,4 @@
 // Add support for decentralized lending platforms
 // Implement price feed oracles for tokens
 // Add gas optimization features for smart contracts
+// Optimize WASM compilation for Go contracts
